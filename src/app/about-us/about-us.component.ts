1babd3be7c7b21f496dcd18612a08fcab2688048
Alfred-Onuada <aonuada5@gmail.com>
2024-08-19 15:07:28 +0100

    added about us page

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from "../footer/footer.component";
 import { HeaderComponent } from "../header/header.component";
+import { MatDialog } from '@angular/material/dialog';
+import { Title } from '@angular/platform-browser';
+import { VolunteerModalComponent } from '../volunteer-modal/volunteer-modal.component';
 
 @Component({
   selector: 'fuf-about-us',
@@ -10,5 +13,17 @@ import { HeaderComponent } from "../header/header.component";
   styleUrl: './about-us.component.css'
 })
 export class AboutUsComponent {
+  title = 'About Us | Franciso Udofia Foundation';
 
+  constructor(private titleService: Title, public dialog: MatDialog) {
+    this.titleService.setTitle(this.title);
+  }
+
+  openModal(): void {
+    const dialogRef = this.dialog.open(VolunteerModalComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+  }
 }
