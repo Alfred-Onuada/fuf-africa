5206c461f720846ee1347e9e91525f9d1a9a7e3c
Alfred-Onuada <aonuada5@gmail.com>
2024-08-20 06:59:52 +0100

    made header responsive

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,14 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'fuf-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, NgClass],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+  navIsOpen = false;
 }
