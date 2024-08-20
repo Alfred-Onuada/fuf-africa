a31c97b33fe4cd9c32bdbb718adcb3a7ccc6455b
Alfred-Onuada <aonuada5@gmail.com>
2024-08-20 08:15:24 +0100

    update

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -4,11 +4,12 @@ import { HeaderComponent } from "../header/header.component";
 import { MatDialog } from '@angular/material/dialog';
 import { Title } from '@angular/platform-browser';
 import { VolunteerModalComponent } from '../volunteer-modal/volunteer-modal.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'fuf-about-us',
   standalone: true,
-  imports: [FooterComponent, HeaderComponent],
+  imports: [FooterComponent, HeaderComponent, RouterLink],
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.css'
 })
